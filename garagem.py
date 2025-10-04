3912cfebce72b11f0d6af7e893b22ed93215caf7
João Costa <123891296+joaocosta956@users.noreply.github.com>
2025-10-04 15:16:39 -0300

    Update garagem.py

--- garagem.py
+++ garagem.py
@@ -19,12 +19,30 @@ class Marca:
         self.nome = nome
         self.pais_origem = pais_origem
 
+    def __repr__(self):
+        return f'{self.nome, self.pais_origem}'
+
 
 class Veiculo:
     def __init__(self, placa, modelo, marca) -> None:
-        self.placa = placa
-        self.modelo = modelo
-        self.marca = marca
+        self._placa = placa
+        self._modelo = modelo
+        self._marca = marca
+
+        @property
+        def placa(self):
+            return self._placa
+
+        @property
+        def modelo(self):
+            return self._modelo
+
+        @property
+        def marca(self):
+            return self._marca
+
+    def __repr__(self) -> str:
+        return f'{self._modelo,self._marca}'
 
 
 class Carro(Veiculo):
@@ -35,9 +53,13 @@ class Carro(Veiculo):
         self.quilometros = quilometros
 
 
+marcas = {
+    'HONDA': Marca('Honda', 'Japão'),
+    'VOLKSWAGEM': Marca('Volkswagem', 'Alemanha'),
+}
+
 placas = {
-    # Aqui fica as placas, modelo e marca placa [modelo, marca]
-    'AAAA1': ['Civic', 'Honda'],
+    # Aqui fica as placas, modelo e marca placa [modelo, Obj Marca]
 
 }
 
@@ -51,21 +73,58 @@ def consulta_placa():
     # A informação é recebida de dentro da função, ela não recebe parametros
     placa = input('\nInsira o número da placa: ').upper()
     if placa in placas:
-        for x in placas[placa]:
-            print(x)
+        print(placas[placa])
     else:
         print('Não há esta placa!')
+
+
+def consulta_marcas():
+    ...
+
+# FUNCAO QUE ADICIONA MARCAS
+
+
+def adiciona_marca():
+    marca_input = input("Insira o nome da Marca: ").upper()
+# Primeiro ve se ja n existe esta marca
+    if marca_input in marcas:
+        print('Marca já existente')
+        return None
+# Se n existir ele pede o pais
+    else:
+        pais_input = input("Insira o país de origem: ")
+        nova_marca = Marca(
+            nome=marca_input,
+            pais_origem=pais_input,
+        )
+        marcas[marca_input] = nova_marca
 
 
 def adiciona_placa():
     # Parametros definidos dentro da função via input, mas poderia coletar
     # fora também, fiz assim pois evita bastante if's
-    modelo = input('Insira o modelo do carro: ')
-    marca = input('Insira a marca do carro: ')
-    placa = input('Insira a placa: ')
-    if modelo and marca and placa:
-        placa = placa.upper().strip()
-        placas[placa] = [modelo, marca]
+    modelo_input = input('Insira o modelo do carro: ')
+    marca_input = input('Insira a marca do carro: ').upper()
+    # Vai conferir se a marca existe
+    if marca_input not in marcas:
+        print('Marca não existente')
+        return None
+
+    placa_input = input('Insira a placa: ')
+    # Vai conferir se a placa já existe
+    if placa_input in placas:
+        print('Placa já existente.')
+        return False
+
+    marca_obj = marcas[marca_input]
+
+    novo_carro = Carro(
+        placa=placa_input,
+        modelo=modelo_input,
+        marca=marca_obj
+    )
+
+    placas[placa_input] = novo_carro
 
 
 def consulta_todas():
@@ -88,8 +147,8 @@ opcoes = {
     'C': consulta_placa,
     'L': terminal_clean,
     'A': consulta_todas,
-    '': ...,
-    '': ...,
+    'M': adiciona_marca,
+    'S': exit,
     '': ...,
 }
 
